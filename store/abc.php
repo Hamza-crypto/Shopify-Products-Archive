427b64061253a5d99313e8a87d78033724162e9a
hamza-crypto <6793sidique@gmail.com>
2023-01-06 03:04:42 +0500

    Archived done

--- store/abc.php
+++ store/abc.php
@@ -1,1 +1,117 @@
 <?php
+
+require_once __DIR__ . '/includes/init.php';
+global $log;
+global $con;
+
+$shop = $_ENV['SHOPIFY_SHOP_SLUG'];
+$token = $_ENV['SHOPIFY_ACCESS_TOKEN'];
+$query = array('Content-type' => 'application/json');
+
+$log->info('orders:start');
+
+$query = mysqli_query($con, sprintf('SELECT * FROM orders where shop="%s"', $shop));
+while ($row = mysqli_fetch_assoc($query)) {
+    mysqli_query($con, sprintf('DELETE FROM line_items WHERE order_id=%s', $row['id']));
+    mysqli_query($con, sprintf('DELETE FROM orders WHERE id=%s', $row['id']));
+}
+
+$page_info = '';
+$filters = array(
+    'fields' => 'name,current_total_price,source_name,created_at,line_items',
+    'limit' => 250,
+    'status' => 'closed',
+    'financial_status' => 'paid'
+);
+
+$i = 0;
+while (true) {
+    $orders = shopify_call(
+        $token,
+        $shop,
+        '/admin/api/2022-04/orders.json',
+        $filters
+    );
+
+    $headers = $orders['headers'];
+    $orders = json_decode($orders['data'], TRUE);
+    $log->info('orders:fetched', [count($orders['orders'])]);
+
+    if (count($orders['orders']) <= 0) {
+        $log->info('zero:orders', $orders);
+    }
+
+    foreach ($orders['orders'] as $order) {
+        $query = mysqli_query(
+            $con,
+            sprintf(
+                'INSERT INTO orders(shop, name, current_total_price, created_at) VALUES("%s", "%s", "%s", "%s")',
+                mysqli_escape_string($con, $shop),
+                mysqli_escape_string($con, $order['name']),
+                $order['current_total_price'],
+                $order['created_at']
+            )
+        );
+
+        if (!$query) {
+            $log->error(mysqli_error($con));
+        }
+
+        $order_id = mysqli_insert_id($con);
+        foreach ($order['line_items'] as $line_item) {
+            $query = mysqli_query(
+                $con,
+                sprintf(
+                    'INSERT INTO line_items(order_id, name, price, source_name, created_at) VALUES(%d, "%s", %0.2f, "%s", "%s")',
+                    $order_id,
+                    mysqli_escape_string($con, $line_item['name']),
+                    $line_item['price'],
+                    $order['source_name'] ?? '',
+                    $order['created_at']
+                )
+            );
+
+            if (!$query) {
+                $log->error(mysqli_error($con));
+            }
+        }
+    }
+
+    if (isset($headers['link'])) {
+        if ($i == 0) {
+            $expression = preg_match_all(
+                '/&page_info=(.*?)>; rel="next"/m',
+                $headers['link'],
+                $matches,
+                PREG_SET_ORDER,
+                0
+            );
+        } else {
+            $expression = preg_match_all(
+                '/rel="previous".*&page_info=(.*?)>; rel="next"/m',
+                $headers['link'],
+                $matches,
+                PREG_SET_ORDER,
+                0
+            );
+        }
+
+        if (!isset($matches[0])) {
+            break;
+        }
+
+        $filters = array(
+            'fields' => 'name,current_total_price,source_name,created_at,line_items',
+            'limit' => 250,
+            'page_info' => $matches[0][1],
+            'rel' => 'next'
+        );
+    } else {
+        break;
+    }
+
+    $i++;
+    sleep(2);
+}
+
+$log->info('orders:end');
--- store/products.php
+++ store/products.php
@@ -10,54 +10,137 @@ $query = array('Content-type' => 'application/json');
 
 $log->info('orders:start');
 
-$page_info = '';
-$filters = [
-    'limit' => 250,
+$page_info = null;
+if (isset($_GET['page_info'])) {
+    $page_info = $_GET['page_info'];
+
+    $filters = array(
+        'limit' => 250,
+        'page_info' => $page_info,
+        'rel' => 'next'
+    );
+}
+else{
+    $filters = array(
+        'limit' => 250,
+        'status' => 'active',
+    );
+}
+
+
+
+$body = [
+    'product' => ['status' => 'draft']
 ];
 
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $handle = fopen($_FILES['file']['tmp_name'], "r");
-    $headers = fgetcsv($handle, 1000, ",");
-    $sku = [];
-    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE)
-    {
-        $sku[] = $data[0];
+
+    $upc = [];
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $upc[] = $data[0];
     }
     fclose($handle);
-    $sku = array_unique($sku);
+    $upc = array_unique($upc);
+    $i = 0;
+    while (true) {
+        $products = shopify_call(
+            $token,
+            $shop,
+            '/admin/api/2023-01/products.json',
+            $filters
+        );
 
-    $products = shopify_call(
-        $token,
-        $shop,
-        '/admin/api/2023-01/products.json',
-        $filters
-    );
-    dump($products);
-    $products = json_decode($products['data'], TRUE);
-    $log->info('orders:fetched', [$products]);
-    $log->info('orders:fetched', [count($products['products'])]);
+        $headers = $products['headers'];
+        $products = json_decode($products['data'], TRUE);
+//        $log->info('orders:fetched', [$products]);
+//        $log->info('orders:fetched', [count($products['products'])]);
 
-    if (count($products['products']) <= 0) {
-        $log->info('zero:orders', $products);
-    }
-
-    $count = 1;
-    foreach ($products['products'] as $order) {
-        dump($count . ' ------  order ID: ' .  $order['id']);
-        $count++;
-        foreach ($order['variants'] as $variant) {
-            if(in_array($variant['sku'], $sku)) {
-                echo 'SKU found';
-            } else {
-                echo 'SKU not found';
-            }
-            dump( $variant['sku']);
+        if (count($products['products']) <= 0) {
+            $log->info('zero:orders', $products);
         }
+
+
+        $count = 0;
+        $key = 'barcode';
+        foreach ($products['products'] as $product) {
+            foreach ($product['variants'] as $variant) {
+
+                if (isset($variant[$key])) {
+
+                    if (in_array($variant[$key], $upc)) {
+                        //Make this product archive
+                        $status = shopify_call(
+                            $token,
+                            $shop,
+                            sprintf('/admin/api/2023-01/products/%s.json', $product['id']),
+                            $body,
+                            'PUT'
+                        );
+                        echo sprintf('Product ID: %s, UPC: %s</br>', $product['id'], $variant[$key]);
+                        $count++;
+                    }
+                } else {
+                    // Delete this product
+//                echo
+
+                }
+
+            }
+        }
+        if (isset($headers['link'])) {
+            if (is_null($page_info) && $i == 0) {
+                $expression = preg_match_all(
+                    '/&page_info=(.*?)>; rel="next"/m',
+                    $headers['link'],
+                    $matches,
+                    PREG_SET_ORDER,
+                    0
+                );
+            } else {
+                $expression = preg_match_all(
+                    '/rel="previous".*&page_info=(.*?)>; rel="next"/m',
+                    $headers['link'],
+                    $matches,
+                    PREG_SET_ORDER,
+                    0
+                );
+            }
+
+            if (!isset($matches[0])) {
+                dump($headers);
+                echo "No more products and breaking the loop";
+                break;
+            }
+
+            $filters = array(
+                'limit' => 250,
+                'page_info' => $matches[0][1],
+                'rel' => 'next'
+            );
+            $log->info('Pagination', [sprintf('Count: %s, Next Page: %s</br>', $i, $matches[0][1])]);
+        } else {
+            dump($headers);
+            echo "No more products and breaking the loop";
+            break;
+        }
+
+        $i++;
+
+        if($i == 40) {
+            echo "Breaking the loop, You can continue from next page by changing the page_info";
+            dump(sprintf('Count: %s', $i));
+            dump($matches[0][1]);
+            break;
+        }
+
+
+        sleep(2);
     }
 
+    echo "Total products archived: " . $count;
 }
-
 $log->info('orders:end');
 
 ?>
